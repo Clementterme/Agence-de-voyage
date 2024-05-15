e770df45c14bd2c9e0a52a1dcd3da5e878e8c36b
Clément <clement.terme.simplon@gmail.com>
2024-05-15 11:53:36 +0200

    Fetch pour afficher les voyages en front

--- agencevoyagefrontend/src/app/voyages/page.jsx
+++ agencevoyagefrontend/src/app/voyages/page.jsx
@@ -2,13 +2,38 @@
 
 import Navbar from "@/components/navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { useEffect, useState } from "react";
 
 export default function Voyages() {
-    return (
-        <>
-        <Navbar />
-        Page des voyages
-        </>
-    );
+  const [loading, setLoading] = useState(true); // État de chargement des données.
+  const [error, setError] = useState(false); // État pour capturer une éventuelle erreur lors du fetch.
+  const [data, setData] = useState(null); // Stockage des données reçues du fetch.
+
+  useEffect(() => {
+    // Déclenchement de la récupération des données de personnages au montage du composant.
+    try {
+      // fetch("https://rickandmortyapi.com/api/character")
+      fetch("http://127.0.0.1:8000/api/voyages")
+        .then((response) => response.json()) // Transformation de la réponse en JSON.
+        .then((data) => {
+          setLoading(false); // Arrêt de l'indicateur de chargement après la réception des données.
+          setData(data.results); // Enregistrement des données reçues dans l'état 'data'.
+          console.log(data);
+        });
+    } catch (error) {
+      setError(true); // Enregistrement de l'erreur dans l'état 'error'.
+      setLoading(false); // Arrêt de l'indicateur de chargement en cas d'erreur.
+    }
+  }, []); // Le tableau vide indique que cet effet ne s'exécute qu'au montage.
+
+  return (
+    <main>
+      <Navbar />
+      {/* Affichage conditionnel en fonction de l'état du chargement et des erreurs */}
+      {loading && !error && <div>Données en cours de chargement !</div>}
+      {/* {!loading && !error && data && <CharacterList characters={data} />} */}
+      {!loading && !error && data && console.log(data)}
+      {!loading && error && <div>Une erreur est survenue</div>}
+    </main>
+  );
 }
